477cf36954236ceecbbb325f546a6ca3303cf774
Jaqueline <59448111+Jaque-81@users.noreply.github.com>
2022-06-04 16:13:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 let bd = []
 
-const getBD = () => JSON.parse(localStorage.getItem('todoList')) ?? []
-const setBD = bd => localStorage.setItem('todoList', JSON.stringify(bd))
+const getBD = () => JSON.parse(localStorage.getItem('todoList')) ?? []; //JSON.parse> transformando em ARRAY //se tiver alguma coisa na variavel todolist pega, senão '??'> se o getBD estiver vazio, passa um array que vai ser passado
+const setBD = bd => localStorage.setItem('todoList', JSON.stringify(bd));
 
 const criarTafera = (tarefa, status, indice) => {
-  const item = document.createElement('label') //cria um label
-  item.classList.add('todo__item')
+  const item = document.createElement('label'); //cria um label
+  item.classList.add('todo__item');
   item.innerHTML = /* dentro do HTML*/ `
         <input type="checkbox" ${status} data-indice=${indice}> 
         <div>${tarefa}</div>
@@ -18,13 +18,13 @@ const criarTafera = (tarefa, status, indice) => {
 const limparTarefas = () => {
   const todoList = document.getElementById('todoList')
   while (todoList.firstChild) {
-    todoList.removeChild(todoList.lastChild)
+    todoList.removeChild(todoList.lastChild);
   }
 }
 
 const atualizarTela = () => {
-  limparTarefas()
-  const bd = getBD()
+  limparTarefas();
+  const bd = getBD();
   bd.forEach((item, indice) => criarTafera(item.tarefa, item.status, indice))
 }
 
@@ -32,42 +32,42 @@ const inserirItem = evento => {
   const tecla = evento.key
   const texto = evento.target.value
   if (tecla === 'Enter') {
-    const bd = getBD()
+    const bd = getBD();
     bd.push({ tarefa: texto, status: '' })
-    setBD(bd)
-    atualizarTela()
-    evento.target.value = '' //apaga a caixa
+    setBD(bd);
+    atualizarTela();
+    evento.target.value = '' ;//apaga a caixa
   }
 }
 
 const removerItem = indice => {
-  const bd = getBD()
+  const bd = getBD();
   bd.splice(indice, 1) //splice recorta/modifica um array
-  setBD(bd)
-  atualizarTela()
+  setBD(bd);
+  atualizarTela();
 }
 
 const atualizarItem = indice => {
-  const bd = getBD()
+  const bd = getBD();
   bd[indice].status = bd[indice].status === '' ? 'checked' : ''
-  setBD(bd)
-  atualizarTela()
+  setBD(bd);
+  atualizarTela();
 }
 
 const clickTarefa = evento => {
   const elemento = evento.target
-  console.log(elemento.type)
+  console.log(elemento.type);
   if (elemento.type === 'button') {
     const indice = elemento.dataset.indice
-    removerItem(indice)
+    removerItem(indice);
   } else if (elemento.type === 'checkbox') {
     const indice = elemento.dataset.indice
-    atualizarItem(indice)
-  }
+    atualizarItem(indice);
+  } 
 }
 
-document.getElementById('newItem').addEventListener('keypress', inserirItem) // vou no id newItem e capturo o evento keypress// addEventListener manda pro callback 'inserirTarefa' o evento que aconteceu em inserirTarefa.
+document.getElementById('newItem').addEventListener('keypress', inserirItem); // vou no id newItem e capturo o evento keypress// addEventListener manda pro callback 'inserirTarefa' o evento que aconteceu em inserirTarefa.
 //document.getElementById('todoList').addEventListener('click', clickTarefa);
-document.getElementById('todoList').addEventListener('click', clickTarefa)
+document.getElementById('todoList').addEventListener('click', clickTarefa);
 
-atualizarTela()
+atualizarTela();
